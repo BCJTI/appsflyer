c5692aa65f093ea1fe3a89c41d602978cb71932d
Marcelo Monaco <marcelo.monaco@gmail.com>
2018-03-08 11:08:37 -0300

    A better version of StringToInterface

--- format.go
+++ format.go
@@ -2,11 +2,8 @@ package appsflyer
 
 import (
 	"time"
-	"regexp"
 	"strings"
 	"strconv"
-	"github.com/asaskevich/govalidator"
-	"github.com/metakeule/fmtdate"
 )
 
 const (
@@ -19,20 +16,40 @@ func ParseDateTimeFormat(v string) (time.Time, error) {
 
 func StringToInterface(str string) (value interface{}){
 
-	// create regex for data validation
-	isDate := regexp.MustCompile(`^\d{4}-\d{2}-\d{2}$`)
+	var err error
+
+	if value, err = strconv.ParseInt(str, 10, 64); err == nil {
+		return
+	}
+
+	if value, err = strconv.ParseFloat(str,64); err == nil {
+		return
+	}
+
+	if len(str) > 1 {
+		if value, err = strconv.ParseBool(str); err == nil {
+			return
+		}
+	}
+
+	if value, err = time.Parse("2006-01-02", str); err == nil {
+		return
+	}
+
+	if value, err = time.Parse("2006-01-02 15:04:05", str); err == nil {
+		return
+	}
+
+	if value, err = time.Parse("2006-01-02T15:04:05", str); err == nil {
+		return
+	}
+
+	if value, err = time.Parse(time.RFC3339, str); err == nil {
+		return
+	}
 
 	// string as default
 	value = strings.Trim(str," ")
-
-	// convert data type
-	if govalidator.IsInt(str) {
-		value, _ = strconv.ParseInt(str, 10, 64, )
-	} else if govalidator.IsFloat(str) {
-		value, _ = strconv.ParseFloat(str,64)
-	} else if isDate.MatchString(str) {
-		value, _ = fmtdate.ParseDate(str)
-	}
 
 	return
 
