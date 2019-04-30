eafc939c8608f979c35243a5ded9fd54a1514e36
Marcelo Monaco <marcelo.monaco@gmail.com>
2019-04-30 11:21:52 -0300

    Comments

--- const.go
+++ const.go
@@ -1,25 +1,29 @@
 package appsflyer
 
 const (
-	AppsFlyerVersion = "v5"
+	AppsFlyerVersion  = "v5"
 	defaultAPIBaseURL = "https://hq.appsflyer.com"
 )
 
 // https://support.appsflyer.com/hc/en-us/articles/207034346-Pull-APIs-Pulling-AppsFlyer-Reports-by-APIs
+// https://support.appsflyer.com/hc/en-us/articles/207034216-Partners-Media-Source-Report
+// https://support.appsflyer.com/hc/en-us/articles/207034256-Partners-by-Date-Report
+// https://support.appsflyer.com/hc/en-us/articles/207034226-Geo-Performance-Report
+
 const (
 	// Performance Reports
-	PartnersReport           = "partners_report/"
-	PartnersDailyReport      = "partners_by_date_report/"
-	DailyReport              = "daily_report/"
-	GeoReport                = "geo_report/"
-	GeoDailyReport           = "geo_by_date_report/"
+	PartnersReport      = "partners_report/"
+	PartnersDailyReport = "partners_by_date_report/"
+	DailyReport         = "daily_report/"
+	GeoReport           = "geo_report/"
+	GeoDailyReport      = "geo_by_date_report/"
 
 	// Raw Data Reports
-	InstallsReport           = "installs_report/"
-	InAppEventsReport        = "in_app_events_report/"
-	UninstallsReport         = "uninstall_events_report/"
-	OrganicInstallsReport    = "organic_installs_report/"
-	OrganicInAppReport       = "organic_in_app_events_report/"
+	InstallsReport        = "installs_report/"
+	InAppEventsReport     = "in_app_events_report/"
+	UninstallsReport      = "uninstall_events_report/"
+	OrganicInstallsReport = "organic_installs_report/"
+	OrganicInAppReport    = "organic_in_app_events_report/"
 
 	// Protect360 Reports
 	BlockedInstallsReport    = "blocked_installs_report/"
